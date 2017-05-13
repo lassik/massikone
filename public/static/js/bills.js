f897fa67d774a2f7f5acf832c36e2b16416ae88a
Lassi Kortela <lassi@lassi.io>
2017-05-13 11:04:58 +0300

    Remove leftover "Tags" modal JS

--- public/static/js/bills.js
+++ public/static/js/bills.js
@@ -15,19 +15,4 @@ $(function() {
         $(".tag"+tag_index).toggle();
     });
 
-    $("#tags-modal").on("shown.bs.modal", function () {
-        $.get({
-            url: "/api/tags"
-        }).done(function(tags) {
-            $("#tag-list").empty();
-            tags.forEach(function (tag) {
-                var li = $("<li>").appendTo($("#tag-list"));
-                li.append(tag.tag);
-                li.append($('<button class="btn btn-info" type="button"><span class="glyphicon glyphicon-remove"></span></button>'));
-            });
-        }).fail(function(jqXHR) {
-            alert("Error: " + jqXHR.statusText);
-        });
-    });
-
 });
