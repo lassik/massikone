dbf13921f5f95963cebee8efc570211a43bbf103
Lassi Kortela <lassi@lassi.io>
2018-05-26 15:29:13 +0300

    Rename 'public' -> 'static'

--- massikone-build.sh
+++ massikone-build.sh
@@ -5,7 +5,7 @@ export GOPATH=$PWD/.go
 export PATH=$GOPATH/bin:$PATH
 export CGO_ENABLED=1  # Required for sqlite3
 set -x
-! which cloc >/dev/null 2>&1 || cloc *.go model reports views public
+! which cloc >/dev/null 2>&1 || cloc massikone.go model reports static views
 packr
 go build -o massikone
 packr clean
