bd02716c122ba64122a42d266a538428b4b0b3bb
Lassi Kortela <lassi@lassi.io>
2018-05-26 15:29:30 +0300

    Reformat

--- massikone-build.sh
+++ massikone-build.sh
@@ -3,7 +3,7 @@ set -eux
 cd "$(dirname "$0")"
 export GOPATH=$PWD/.go
 export PATH=$GOPATH/bin:$PATH
-export CGO_ENABLED=1  # Required for sqlite3
+export CGO_ENABLED=1 # Required for sqlite3
 set -x
 ! which cloc >/dev/null 2>&1 || cloc massikone.go model reports static views
 packr
