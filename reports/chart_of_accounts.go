8020a1ebda398a4bf3237a73c92e196c50e54b64
Lassi Kortela <lassi@lassi.io>
2018-12-05 00:32:39 +0200

    Implement chart of accounts report

--- reports/chart_of_accounts.go
+++ reports/chart_of_accounts.go
@@ -1,9 +1,43 @@
 package reports
 
 import (
+	"strconv"
+
 	"github.com/lassik/massikone/model"
 )
 
 func ChartOfAccountsPdf(m *model.Model, getWriter GetWriter) {
-	blankPdf(getWriter, "tilikartta")
+	accounts := m.GetAccounts(false, "")
+	doc := document{
+		orgName:   model.GetSettings().OrgShortName,
+		title:     "Tilikartta",
+		filename:  "tilikartta",
+		period:    "1.1.2018 - 31.12.2018",
+		printDate: "1.12.2018",
+	}
+	for _, acct := range accounts {
+		level := 0
+		if acct.HTagLevel != "" {
+			level, _ = strconv.Atoi(acct.HTagLevel)
+		}
+		if level < 1 {
+			level = 9
+		}
+		bold := level < 9
+		thisRow := []cell{
+			cell{
+				text:  acct.AccountID,
+				bold:  bold,
+				width: 1,
+			},
+			cell{
+				text:        acct.Title,
+				bold:        bold,
+				indentLevel: level,
+				width:       10,
+			},
+		}
+		doc.rows = append(doc.rows, thisRow)
+	}
+	writePdf(doc, getWriter)
 }
