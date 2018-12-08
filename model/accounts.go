f83133a8cfba0c0380fb45c53acf75b6798f834c
Lassi Kortela <lassi@lassi.io>
2018-12-08 13:42:25 +0200

    Populate account list and account map the same way

--- model/accounts.go
+++ model/accounts.go
@@ -29,39 +29,50 @@ type Account struct {
 	IsMatch      bool
 }
 
+func selectAccount() sq.SelectBuilder {
+	return sq.Select("account_id, account_type, title, nesting_level").
+		From("period_account").
+		OrderBy("account_id, nesting_level")
+}
+
+func scanAccount(rows sq.RowScanner) (Account, error) {
+	var a Account
+	var nestingLevel int
+	if err := rows.Scan(&a.AccountID, &a.AccountType,
+		&a.Title, &nestingLevel); err != nil {
+		return a, err
+	}
+	isAccount := (nestingLevel == accountNestingLevel)
+	dashLevel := 0
+	if !isAccount {
+		dashLevel = 1 + nestingLevel
+		a.HTagLevel = strconv.Itoa(2 + nestingLevel)
+	}
+	if isAccount {
+		a.AccountIDStr = strconv.Itoa(a.AccountID)
+		a.Prefix = a.AccountIDStr
+	} else {
+		a.Prefix = strings.Repeat("=", dashLevel)
+	}
+	return a, nil
+}
+
 func (m *Model) GetAccounts(usedOnly bool, matchAccountID string) []Account {
 	noAccounts := []Account{}
 	accounts := noAccounts
-	//populateAccounts()
-	rows, err := sq.Select("account_id, account_type, title, nesting_level").
-		From("period_account").OrderBy("account_id, nesting_level").
-		RunWith(m.tx).Query()
+	rows, err := selectAccount().RunWith(m.tx).Query()
 	if m.isErr(err) {
 		return noAccounts
 	}
 	defer rows.Close()
 	for rows.Next() {
-		var a Account
-		var nestingLevel int
-		if m.isErr(rows.Scan(&a.AccountID, &a.AccountType,
-			&a.Title, &nestingLevel)) {
+		acct, err := scanAccount(rows)
+		if m.isErr(err) {
 			return noAccounts
 		}
-		isAccount := (nestingLevel == accountNestingLevel)
-		dashLevel := 0
-		if !isAccount {
-			dashLevel = 1 + nestingLevel
-			a.HTagLevel = strconv.Itoa(2 + nestingLevel)
-		}
-		if isAccount {
-			a.AccountIDStr = strconv.Itoa(a.AccountID)
-			a.Prefix = a.AccountIDStr
-		} else {
-			a.Prefix = strings.Repeat("=", dashLevel)
-		}
-		a.IsMatch = (matchAccountID != "" &&
-			a.AccountIDStr == matchAccountID)
-		accounts = append(accounts, a)
+		acct.IsMatch = (matchAccountID != "" &&
+			acct.AccountIDStr == matchAccountID)
+		accounts = append(accounts, acct)
 	}
 	if usedOnly {
 		//accounts = reject_unused_accounts(accounts, @db[:bill_entry].select(:account_id).order(:account_id).distinct.map(:account_id))
@@ -71,18 +82,17 @@ func (m *Model) GetAccounts(usedOnly bool, matchAccountID string) []Account {
 
 func (m *Model) GetAccountMap() map[int]Account {
 	acctMap := map[int]Account{}
-	rows, err := sq.Select("account_id, title").
-		From("period_account").OrderBy("account_id").
-		Where(sq.Eq{"nesting_level": accountNestingLevel}).
-		RunWith(m.tx).Query()
+	rows, err := selectAccount().RunWith(m.tx).Query()
 	if m.isErr(err) {
 		return acctMap
 	}
 	defer rows.Close()
 	for rows.Next() {
-		var acctID int
-		acct := Account{}
-		rows.Scan(&acctID, &acct.Title)
+		acct, err := scanAccount(rows)
+		if m.isErr(err) {
+			break
+		}
+		acctID, _ := strconv.Atoi(acct.AccountIDStr)
 		acctMap[acctID] = acct
 	}
 	return acctMap
